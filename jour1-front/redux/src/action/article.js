b337e62cb9ce175a1d492f2b0e918069c26f0bbf
Malik H <malik.h@webdevpro.net>
2021-12-20 17:09:17 +0100

    ajouter une nouvelle action dans action

--- jour1-front/redux/src/action/article.js
+++ jour1-front/redux/src/action/article.js
@@ -1,11 +1,20 @@
 import axios from "axios";
 
 export const GET_ARTICLES = "GET_ARTICLES";
+export const GET_NB_ARTICLES = "GET_NB_ARTICLES"; 
 
 export const getArticles = () => {
     return (dispatch) => {
         return axios.get("http://localhost:3000/articles")
                 .then( (res) => dispatch({ type: GET_ARTICLES , payload : res.data }))
+                .catch( ex  => console.log(ex))
+    }
+}
+
+export const getNbArticles = () => {
+    return (dispatch) => {
+        return axios.get("http://localhost:3000/articles")
+                .then( (res) => dispatch({ type: GET_NB_ARTICLES , payload : res.data.length }))
                 .catch( ex  => console.log(ex))
     }
 }
